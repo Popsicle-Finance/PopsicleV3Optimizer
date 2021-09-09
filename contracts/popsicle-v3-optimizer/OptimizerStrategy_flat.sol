4e95d149125da1415e3bf5bb874b38fea1b99c83
flachy <vt@bitreezer.app>
2021-09-09 11:54:23 +0300

    fix twap params

--- contracts/popsicle-v3-optimizer/OptimizerStrategy_flat.sol
+++ contracts/popsicle-v3-optimizer/OptimizerStrategy_flat.sol
@@ -40,6 +40,7 @@ contract OptimizerStrategy is IOptimizerStrategy {
     /// @inheritdoc IOptimizerStrategy
     uint24 public override priceImpactPercentage;
     
+    event TransferGovernance(address indexed previousGovernance, address indexed newGovernance);
     
     /**
      * @param _twapDuration TWAP duration in seconds for rebalance check
@@ -62,8 +63,8 @@ contract OptimizerStrategy is IOptimizerStrategy {
         maxTotalSupply = _maxTotalSupply;
         governance = msg.sender;
 
-        require(_maxTwapDeviation >= 0, "maxTwapDeviation");
-        require(_twapDuration > 0, "twapDuration");
+        require(_maxTwapDeviation >= 20, "maxTwapDeviation");
+        require(_twapDuration >= 100, "twapDuration");
         require(_priceImpactPercentage < 1e6 && _priceImpactPercentage > 0, "PIP");
         require(maxTotalSupply > 0, "maxTotalSupply");
     }
@@ -79,12 +80,12 @@ contract OptimizerStrategy is IOptimizerStrategy {
     }
 
     function setTwapDuration(uint32 _twapDuration) external onlyGovernance {
-        require(_twapDuration > 0, "twapDuration");
+        require(_twapDuration >= 100, "twapDuration");
         twapDuration = _twapDuration;
     }
 
     function setMaxTwapDeviation(int24 _maxTwapDeviation) external onlyGovernance {
-        require(_maxTwapDeviation > 0, "PF");
+        require(_maxTwapDeviation >= 20, "PF");
         maxTwapDeviation = _maxTwapDeviation;
     }
 
@@ -112,6 +113,8 @@ contract OptimizerStrategy is IOptimizerStrategy {
      */
     function acceptGovernance() external {
         require(msg.sender == pendingGovernance, "PG");
+        emit TransferGovernance(governance, pendingGovernance);
+        pendingGovernance = address(0);
         governance = msg.sender;
     }
 }
