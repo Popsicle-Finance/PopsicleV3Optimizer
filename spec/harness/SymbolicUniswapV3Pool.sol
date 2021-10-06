462887871fe307e38583d6208370f09b96bef879
Gadi Rechlis <gadi@certora.com>
2021-10-06 09:09:00 +0300

    safe downcasting

--- spec/harness/SymbolicUniswapV3Pool.sol
+++ spec/harness/SymbolicUniswapV3Pool.sol
@@ -149,6 +149,9 @@ contract SymbolicUniswapV3Pool is IUniswapV3Pool {
         amount0 = amount;
         amount1 = amount * ratio;
         liquidity -= amount;
+        
+        require (amount0 < 2**128);
+        require (amount1 < 2**128);
         owed0 += uint128(amount0);
         owed1 += uint128(amount1);
     }
@@ -214,14 +217,14 @@ contract SymbolicUniswapV3Pool is IUniswapV3Pool {
     /// @notice Returns the cumulative tick and liquidity as of each timestamp `secondsAgo` from the current block timestamp
     /// @dev To get a time weighted average tick or liquidity-in-range, you must call this with two values, one representing
     /// the beginning of the period and another for the end of the period. E.g., to get the last hour time-weighted average tick,
-    /// you must call it with secondsAgos = [3600, 0].
+    /// you must call it with secondsAgo = [3600, 0].
     /// @dev The time weighted average tick represents the geometric time weighted average price of the pool, in
     /// log base sqrt(1.0001) of token1 / token0. The TickMath library can be used to go from a tick value to a ratio.
-    /// @param secondsAgos From how long ago each cumulative tick and liquidity value should be returned
-    /// @return tickCumulatives Cumulative tick values as of each `secondsAgos` from the current block timestamp
-    /// @return secondsPerLiquidityCumulativeX128s Cumulative seconds per liquidity-in-range value as of each `secondsAgos` from the current block
+    /// @param secondsAgo From how long ago each cumulative tick and liquidity value should be returned
+    /// @return tickCumulatives Cumulative tick values as of each `secondsAgo` from the current block timestamp
+    /// @return secondsPerLiquidityCumulativeX128s Cumulative seconds per liquidity-in-range value as of each `secondsAgo` from the current block
     /// timestamp
-    function observe(uint32[] calldata secondsAgos)
+    function observe(uint32[] calldata secondsAgo)
         external
         view
         override
@@ -230,8 +233,8 @@ contract SymbolicUniswapV3Pool is IUniswapV3Pool {
             uint160[] memory secondsPerLiquidityCumulativeX128s
         )
     {
-        tickCumulatives = new int56[](secondsAgos.length);
-        secondsPerLiquidityCumulativeX128s = new uint160[](secondsAgos.length);
+        tickCumulatives = new int56[](secondsAgo.length);
+        secondsPerLiquidityCumulativeX128s = new uint160[](secondsAgo.length);
     }
 
     /// @notice The pool tick spacing
